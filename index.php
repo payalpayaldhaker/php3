c5b59d57609cedfb6bbc46ed03235a0578d68945
payalpayaldhaker <90671274+payalpayaldhaker@users.noreply.github.com>
2021-11-17 03:22:47 -0800

    php3

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+                echo '<h2>payal dhakad</h2>';
+                echo '<br>';
+                echo '<h1> neemuch</h1>';
+            ?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php tutorial</title>
+        <style> 
+            h1{ 
+                color: red;
+            }
+            h2{ 
+                color: blue;
+            }
+        </style>
+    </head>
+    <body>
+            <?php 
+                echo '<h2> payal dhakad</h2>';
+                echo '<br>';
+                echo '<h1>neemuch</h1>';
+            ?>
+            
+    </body>
+    </html>
+    <?php 
+                echo '<h2>payal</h2>';
+                echo '<br>';
+                echo '<h1>neemuch</h1>';
+            ?>
